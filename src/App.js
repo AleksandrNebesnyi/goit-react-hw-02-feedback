4fe95f06f139779730d6b9178671b4893abe1890
AleksandrNebesnyi <82997944+AleksandrNebesnyi@users.noreply.github.com>
2021-12-13 14:04:35 +0200

    start

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
 
-import Counter from './Counter/Counter.jsx'
 
-function App() {
-  return (
+import Section from "./Section/Section.jsx";
+import Statistics from "./Statistics/Statistics.jsx";
+import {Component} from 'react';
 
 
-    <Counter initialValue={10} />
- 
-  );
+class App extends Component {
+state ={
+  good : 0,
+  neutral :0,
+  bad:0,
 }
+
+countTotalFeedback =()=>{
+  const {good, neutral, bad } = this.state;
+  return good+neutral+bad;
+}
+
+countPositiveFeedbackPercentage=()=>{
+  const { good } = this.state;
+  return Math.floor((good / this.countTotalFeedback()) * 100);
+}
+
+
+  render()  {
+   const {good, neutral , bad } = this.state;
+
+    return (
+      <>
+  
+            <Section title = "ksgksdfgk"/>
+            <Section title="Statistics"/>
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={this.countTotalFeedback} // Передаем в компонент статистики подсчет всех отзывов
+              positivePercentage={this.countPositiveFeedbackPercentage} // Туда же передаем подсчет хороших отзывов
+            />
+        
+     
+   </>
+    );
+  };
+  
+}
+
+
+
 
 export default App;
